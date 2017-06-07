764f8cf08e9b32d2142499a08ff65e7d23f4ae68
Misshel <jenmish094@gmail.com>
2017-06-07 14:51:32 -0500

    mapa con marcador y ruta

--- js/components/gmap.js
+++ js/components/gmap.js
@@ -1,23 +1,51 @@
 const gmap = (update) => {
-    const container = $('<div class="container-fluid "></div>');
-    const row = $('<div class="row row-map"></div>');
-    const map = $('<div id="map" class="col s12 map"></div>');
+  const container = $('<div class="container-fluid "></div>');
+  const row = $('<div class="row row-map"></div>');
+  const map = $('<div id="map" class="col s12 map"></div>');
 
-    row.append(map);
+  row.append(map);
 
-    container.append(row);
-    console.log(state.selectedStation.lat);
+  container.append(row);
+  console.log(state.selectedStation.lat);
 
   $(  _=>{
-      new GMaps({
-        div: '#map',
-        lat: state.selectedStation.lat,
-        lng: state.selectedStation.long
+    const map = new GMaps({
+      div: '#map',
+      lat: state.selectedStation.lat,
+      lng: state.selectedStation.long
+    });
+
+    GMaps.geolocate({
+      success: function(position) {
+        map.setCenter(position.coords.latitude, position.coords.longitude);
+        map.addMarker({
+          lat: position.coords.latitude,
+          lng: position.coords.longitude });
+          map.drawRoute({
+            origin: [position.coords.latitude, position.coords.longitude],
+            destination: [state.selectedStation.lat, state.selectedStation.long],
+            travelMode: 'driving',
+            strokeColor: '#131540',
+            strokeOpacity: 0.6,
+            strokeWeight: 6
+          });
+
+          },
+          error: function(error) {
+            alert('Geolocation failed: '+error.message);
+          },
+          not_supported: function() {
+            alert("Your browser does not support geolocation");
+          },
+          always: function() {
+            alert("Done!");
+          }
+        });
+
+
+
       });
 
-    });
-    // console.log(lat);
+      return container;
 
-    return container;
-
-}
+    }
