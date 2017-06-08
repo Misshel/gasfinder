5466e800f4403ea713b10d91da53128102cfa746
Misshel <jenmish094@gmail.com>
2017-06-07 20:20:13 -0500

    ruta

--- js/components/gmap.js
+++ js/components/gmap.js
@@ -6,7 +6,6 @@ const gmap = (update) => {
   row.append(map);
 
   container.append(row);
-  console.log(state.selectedStation.lat);
 
   $(  _=>{
     const map = new GMaps({
@@ -27,8 +26,14 @@ const gmap = (update) => {
             travelMode: 'driving',
             strokeColor: '#131540',
             strokeOpacity: 0.6,
-            strokeWeight: 6
-          });
+            strokeWeight: 6 });
+
+            const rutaTotal = {origin: [position.coords.latitude, position.coords.longitude],
+                              destination: [state.selectedStation.lat, state.selectedStation.long] };
+
+            const distance = $('<span class="distance">Distancia:<br>' + getDistance (rutaTotal) +'</span>');
+            row.append(distance);
+            console.log(distance.html());
 
           },
           error: function(error) {
@@ -42,10 +47,30 @@ const gmap = (update) => {
           }
         });
 
-
-
       });
 
       return container;
 
     }
+
+
+      const getDistance = (distance) => {
+
+        for(let i=0;i<distance.origin.length;i++){
+          for (let i = 0; i < distance.destination.length; i++) {
+
+            const R = 6371;
+            const dLat = (distance.destination[0]-distance.origin[0]) * (Math.PI/180);
+            const dLon = (distance.destination[1]-distance.origin[1]) * (Math.PI/180);
+
+            const a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+            Math.cos(distance.origin[0] * (Math.PI/180)) * Math.cos(distance.destination[0] * (Math.PI/180)) *
+            Math.sin(dLon/2) * Math.sin(dLon/2);
+
+            const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+            const d = (R * c).toFixed(2)+" km";
+            return (d);
+          }
+        }
+
+      }
